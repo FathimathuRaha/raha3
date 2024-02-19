7efdd24517db7b307b48c6ae2ef78a179af42991
PRASHOB P <prashobp2003.com>
2024-02-19 17:06:51 +0530

    first commit

---
+++ basic.js
@@ -0,0 +1,63 @@
+// let x =10;
+// console.log(x);
+// {
+//     let x=50;
+//     console.log(x);
+// }
+// {
+//     a=true;
+// console.log(a);
+//  console.log(typeof(a));
+// }
+// temp=19;
+// if(temp>30)
+// {
+//     console.log("its a hot day")
+// }
+//  //else if(temp<30 && temp>20)
+//  {
+//     console.log("its a present day");
+
+//  }
+// // else{
+// console.log("its a cold day")
+// //  }
+//  let i=1;
+//  while(i<=15)
+//  {
+//     console.log(i);
+//     if(i%2==0)
+//     {
+//         console.log("even number");
+//     }
+//     else{
+//         console.log("odd number");
+//     }
+//     i++;
+    
+// function greet(name){
+//     console.log("welcome"+name);
+
+// }
+// greet("raha");
+// function add(a,b)
+// {
+//     return a+b;
+// }
+  
+//     console.log(add(2,3));
+// let person=[{
+//     name:"john",
+//     age:19,
+//     city:"kozhikode"
+// },
+// {
+//     name:"nandu",
+//     age:21,
+//    city:"kannur"
+
+// },
+// {
+//     name:"sarima",
+//     age:340,
+//     city:"kasar
